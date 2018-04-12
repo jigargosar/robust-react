66bb2eb7c16a8e4c001fef16085ee92e6488b1b6
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-12 20:51:06 +0530

    feat: add toolbar to layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import {div, h} from './hyper-script'
-import {CssBaseline} from 'material-ui'
+import {AppBar, CssBaseline, Toolbar, Typography, withStyles} from 'material-ui'
 
-const  App = () =>
-  h(CssBaseline, [
-    div('foo')
-  ])
+const App = () => h(CssBaseline, [
+  h(Layout, [
+    h(AppBar, [
+        h(Toolbar, [
+          h(Typography, {variant: 'title', color: 'inherit'}, 'Robust React App'),
+        ]),
+      ],
+    ),
+  ]),
+])
+
+const Layout = withStyles({
+  root: {
+    height: '100vh', width: '100vw', overflow: 'scroll',
+  },
+})(
+  ({children, classes}) =>
+    div({className: classes.root}, [children]),
+)
 
 export default App
