7a63ba2cb8a4d61b7ae30275c0ec2efbc38fec64
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-12 19:49:06 +0530

    chore: make jest work with kcd-scripts. kcd-scripts seems to be tightly coupled to npm

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-testing-library'
 import App from '../App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App/>, div)
-  ReactDOM.unmountComponentAtNode(div)
+  const {getByText, getByTestId, container} = render(<App/>)
+  expect(container.firstChild).toMatchSnapshot()
 })
