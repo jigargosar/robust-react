0706b1bb9e031408f6a267e9fa8fe78fcb0617fc
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-14 17:26:17 +0530

    feat(search): add input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   List,
   MenuItem,
   Paper,
+  TextField,
   Toolbar,
   Typography,
   withStyles,
@@ -38,10 +39,41 @@ const NumberListLayout = withStyles(theme => ({
   ]),
 )
 
-const Header = withStyles({})(() =>
+const Header = withStyles(({spacing: {unit}}) => ({
+  flex: {
+    flex: 1,
+  },
+  textFieldRoot: {
+    // backgroundColor: 'rgba(0,0,0,0.1)',
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    color: 'inherit',
+  },
+  textFieldInput: {
+    margin: `0 ${unit}px`,
+  },
+}))(({classes}) =>
   h(AppBar, {position: 'static'}, [
     h(Toolbar, [
-      h(Typography, {variant: 'title', color: 'inherit'}, 'Robust React App'),
+      h(
+        Typography,
+        {
+          className: classes.flex,
+          variant: 'title',
+          color: 'inherit',
+        },
+        'Robust React App',
+      ),
+      h(TextField, {
+        type: 'search',
+        placeholder: 'Search',
+        InputProps: {
+          disableUnderline: true,
+          classes: {
+            root: classes.textFieldRoot,
+            input: classes.textFieldInput,
+          },
+        },
+      }),
     ]),
   ]),
 )
