4813f96b04f6c68c41afb670c8e51cd347389fb5
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-14 19:51:09 +0530

    feat(search): add setSearchText signal

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,12 +1,7 @@
-import {set} from 'cerebral/operators'
+import {increment as inc, set} from 'cerebral/operators'
 import {props, state} from 'cerebral/tags'
 
-export function increment({state: s}) {
-  s.set('count', state.get('count') + 1)
-}
-
-export function decrement({state: s}) {
-  s.set('count', state.get('count') - 1)
-}
+export const increment = inc(state`searchText`, 1)
+export const decrement = inc(state`searchText`, -1)
 
 export const setSearchText = set(state`searchText`, props`setSearchText`)
