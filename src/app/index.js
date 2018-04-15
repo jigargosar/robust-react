0f53dfa14558fcecef41e64c59a86abc23d624cf
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-15 15:49:31 +0530

    feat(collections): add state

--- src/app/index.js
+++ src/app/index.js
@@ -7,10 +7,15 @@ const initialModels = S.pipe(
   20,
 )
 
+const initialCollections = S.pipe(
+  [S.range(0), S.reverse, S.map(i => ({id: i, text: i + 1}))],
+  20,
+)
 export default Module({
   state: {
     searchText: '',
     models: initialModels,
+    collections: initialCollections,
   },
   signals: {
     updateSearchText: sequences.updateSearchText,
