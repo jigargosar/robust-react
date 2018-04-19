1cbb6f87cfd91c038368a0e77a2b2a2396399daa
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-19 09:07:18 +0530

    feat(model): PaperList

--- src/stories/Collection/index.js
+++ src/stories/Collection/index.js
@@ -1,12 +1,13 @@
 import {storiesOf} from '@storybook/react'
 import Chance from 'chance'
+import {Paper} from 'material-ui'
 import * as R from 'ramda'
-import {h} from '../../hyper-script'
+import {div, h} from '../../hyper-script'
 import ModelList, {ModelListItem} from '../components/ModelListItem'
 
 const createFakeModels = () => {
   const chance = new Chance(11)
-  return R.times(() => ({name: chance.country({full: true})}), 10)
+  return R.times(() => ({name: chance.country({full: true})}), 15)
 }
 
 const models = createFakeModels()
@@ -18,4 +19,11 @@ storiesOf('Models', module)
   })
   .add('List', () => {
     return h(ModelList, {models})
+  })
+  .add('PaperList', () => {
+    return div({style: {textAlign: 'center', backgroundColor: '#ddd'}}, [
+      h(Paper, {style: {margin: 'auto', maxWidth: 450}}, [
+        h(ModelList, {models}),
+      ]),
+    ])
   })
