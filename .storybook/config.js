ea7d536094dfdc8a22fa38925376458fbbc5ebf6
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-19 08:53:59 +0530

    feat(model): list

--- .storybook/config.js
+++ .storybook/config.js
@@ -22,32 +22,32 @@ setOptions({
    * show story component as full screen
    * @type {Boolean}
    */
-  goFullScreen: false,
+  // goFullScreen: false,
   /**
    * display panel that shows a list of stories
    * @type {Boolean}
    */
-  showStoriesPanel: true,
+  // showStoriesPanel: true,
   /**
    * display panel that shows addon configurations
    * @type {Boolean}
    */
-  showAddonPanel: true,
+  // showAddonPanel: true,
   /**
    * display floating search box to search through stories
    * @type {Boolean}
    */
-  showSearchBox: false,
+  // showSearchBox: false,
   /**
    * show addon panel as a vertical panel on the right
    * @type {Boolean}
    */
-  addonPanelInRight: false,
+  // addonPanelInRight: false,
   /**
    * sorts stories
    * @type {Boolean}
    */
-  sortStoriesByKind: false,
+  // sortStoriesByKind: false,
   /**
    * regex for finding the hierarchy separator
    * @example:
@@ -70,12 +70,12 @@ setOptions({
    * sidebar tree animations
    * @type {Boolean}
    */
-  sidebarAnimations: true,
+  // sidebarAnimations: true,
   /**
    * id to select an addon panel
    * @type {String}
    */
-  selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is
+  // selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is
   // the same as you import them in 'addons.js'.
   // The first panel will be opened by default
   // as you run Storybook
