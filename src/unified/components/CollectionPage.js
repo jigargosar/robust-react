28244ff57eba2ccc889f13d55f2815d639e5e9b7
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-24 19:16:46 +0530

    feat(collection): Page list should call onClick with model and event

--- src/unified/components/CollectionPage.js
+++ src/unified/components/CollectionPage.js
@@ -1,23 +1,22 @@
 import Chance from 'chance'
 import {times} from 'ramda'
-import {action, storiesOf} from '../../facade'
+// import 'dom-testing-library/extend-expect'
+import {render, Simulate} from 'react-testing-library'
+import {action, describe, expect, it, storiesOf} from '../../facade'
 import {h} from '../../hyper-script'
 import {ModelList} from './Model'
 import {centerPaper} from './story-decorators/centerPaper'
-// import 'dom-testing-library/extend-expect'
-// import {render} from 'react-testing-library'
 // import {describe, expect, it, specs, storiesOf} from '../../../../facade'
 
-const chance = new Chance(11)
-
-const createCollection = id => ({
+const createCollection = chance => id => ({
   id,
   name: chance.country({full: true}),
   itemCount: chance.integer({min: 3, max: 10}),
 })
 
 const createFakes = () => {
-  return times(createCollection, 3)
+  const chance = new Chance(11)
+  return times(createCollection(chance), 3)
 }
 
 const story = storiesOf('Unified | Page', module).addDecorator(centerPaper)
@@ -36,3 +35,24 @@ story.add('Collection', () =>
       action('collectionClicked')(collection.name, e.type),
   }),
 )
+
+describe('Collection', () => {
+  it('should call onClick with model and event', () => {
+    expect.assertions(4)
+    const onClick = jest.fn(collection => event => {
+      expect(collection.name).toEqual('Kenya')
+      expect(event.type).toEqual('click')
+    })
+    const {getByText} = render(
+      h(Collection, {
+        models: createFakes(),
+        onClick,
+      }),
+    )
+
+    const collectionNode = getByText('Kenya (4)')
+    expect(collectionNode).toBeDefined()
+    Simulate.click(collectionNode)
+    expect(onClick).toHaveBeenCalled()
+  })
+})
