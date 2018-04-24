66af7b5502a089e04968d94aac4d3bfcea72e6ad
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-24 17:12:54 +0530

    feat(collection): create page and model list

---
+++ src/unified/components/CollectionPage.js
@@ -0,0 +1,25 @@
+import {ListItemText, MenuItem} from 'material-ui'
+import {storiesOf} from '../../facade'
+import {h} from '../../hyper-script'
+import {centerPaper} from './story-decorators/centerPaper'
+// import 'dom-testing-library/extend-expect'
+// import {render} from 'react-testing-library'
+// import {describe, expect, it, specs, storiesOf} from '../../../../facade'
+
+const ModelListItem = ({model}) => {
+  return h(MenuItem, [h(ListItemText, model.name)])
+}
+
+// const createFakeModels = () => {
+//   const chance = Chance(11)
+//   return R.times(
+//     (idx: number) => ({id: idx, name: chance.country({full: true})}),
+//     3,
+//   )
+// }
+
+storiesOf('Unified | Page', module)
+  .addDecorator(centerPaper)
+  .add('Collection', () => {
+    return h(ModelListItem, {model: {id: 0, name: 'Foo bar'}})
+  })
--- src/unified/components/Model.js
+++ src/unified/components/Model.js
@@ -1,4 +1,6 @@
-import {ListItemText, MenuItem} from 'material-ui'
+import {Chance} from 'chance'
+import {List, ListItemText, MenuItem} from 'material-ui'
+import {head, map, times} from 'ramda'
 import {storiesOf} from '../../facade'
 import {h} from '../../hyper-script'
 import {centerPaper} from './story-decorators/centerPaper'
@@ -6,20 +8,21 @@ import {centerPaper} from './story-decorators/centerPaper'
 // import {render} from 'react-testing-library'
 // import {describe, expect, it, specs, storiesOf} from '../../../../facade'
 
-const ModelListItem = ({model}) => {
-  return h(MenuItem, [h(ListItemText, model.name)])
+const story = storiesOf('Unified | Model', module).addDecorator(centerPaper)
+
+const createFakes = () => {
+  const chance = Chance(11)
+  return times(
+    (idx: number) => ({id: idx, name: chance.country({full: true})}),
+    3,
+  )
 }
 
-// const createFakeModels = () => {
-//   const chance = Chance(11)
-//   return R.times(
-//     (idx: number) => ({id: idx, name: chance.country({full: true})}),
-//     3,
-//   )
-// }
+const ModelListItem = ({model}) => h(MenuItem, [h(ListItemText, model.name)])
 
-storiesOf('Unified | Model', module)
-  .addDecorator(centerPaper)
-  .add('ListItem', () => {
-    return h(ModelListItem, {model: {id: 0, name: 'Foo bar'}})
-  })
+story.add('ListItem', () => h(ModelListItem, {model: head(createFakes())}))
+
+const ModelList = ({models}) =>
+  h(List, map(model => h(ModelListItem, {model}), models))
+
+story.add('List', () => h(ModelList, {models: createFakes()}))
