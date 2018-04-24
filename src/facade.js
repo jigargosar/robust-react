fb26b30bf18e4be4a94296ccbb62010366dd23d8
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-24 20:12:26 +0530

    feat(collection): simplifying facade code

--- src/facade.js
+++ src/facade.js
@@ -1,43 +1,25 @@
-import {
-  action as actionReal,
-  linkTo as linkToReal,
-  storiesOf as storiesOfReal,
-} from '@storybook/react'
+import expect_ from 'expect'
 
-import x from 'expect'
-
-import {
-  after as afterReal,
-  afterEach as afterEachReal,
-  before as beforeReal,
-  beforeEach as beforeEachReal,
-  describe as describeReal,
-  fit as fitReal,
-  it as itReal,
-  specs as specsReal,
-  xdescribe as xdescribeReal,
-  xit as xitReal,
-} from 'storybook-addon-specifications'
-
-export const storiesOf = storiesOfReal
-export const action = actionReal
-export const linkTo = linkToReal
-export const specs = specsReal
-export const describe = describeReal
-export const it = itReal
-
-export const beforeEach = beforeEachReal
-export const afterEach = afterEachReal
-export const before = beforeReal
-export const after = afterReal
-
-export const xit = xitReal
-export const fit = fitReal
-export const xdescribe = xdescribeReal
-
-x.extend({
+expect_.extend({
   toMatchSnapshot: () => ({message: 'Fake Snapshot For Storybook', pass: true}),
 })
 
-export const expect = x
+export {default as expect} from 'expect'
+
 export {default as jest} from 'jest-mock'
+export {
+  after,
+  afterEach,
+  before,
+  beforeEach,
+  describe,
+  fit,
+  it,
+  specs,
+  xdescribe,
+  xit,
+} from 'storybook-addon-specifications'
+
+export {storiesOf} from '@storybook/react'
+export {action} from '@storybook/addon-actions'
+export {linkTo} from '@storybook/addon-links'
