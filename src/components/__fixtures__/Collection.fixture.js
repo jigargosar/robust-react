24bd6114c7dec6a00bcd3f5c77fa18d496485aae
Jigar Gosar <jigar.gosar@gmail.com>
2018-04-29 15:50:29 +0530

    feat(Collections): add fields to collection

--- src/components/__fixtures__/Collection.fixture.js
+++ src/components/__fixtures__/Collection.fixture.js
@@ -1,11 +1,18 @@
 import Chance from 'chance'
 import {times} from 'ramda'
+import {Collection, StringField} from '../../models/Collection'
 import {createFakeModels} from './Model.fixture'
 
 export const createFakeCollections = () => {
   const chance = new Chance(11)
   return times(
-    idx => ({id: idx, name: chance.city(), items: createFakeModels(chance)}),
+    idx =>
+      Collection({
+        id: idx,
+        name: chance.country({full: true}),
+        fields: [StringField('Tags', 'no-tag')],
+        items: createFakeModels(chance),
+      }),
     3,
   )
 }
--- src/models/Collection.js
+++ src/models/Collection.js
@@ -1,12 +1,27 @@
-import {defaultTo, isNil, when} from 'ramda'
+import assert from 'assert'
+import {contains, curry, defaultTo, isNil, when} from 'ramda'
 import {IdGenerator} from './Id'
 
 const idGen = IdGenerator('Collection', 1)
 
-export const Collection = ({id, name, items} = {}) => {
+const FT_STRING = 'string'
+const FT_BOOL = 'boolean'
+
+const FieldTypes = [FT_STRING, FT_BOOL]
+
+const Field = curry((typeId, name) => {
+  assert(!isNil(name))
+  assert(contains(typeId)(FieldTypes))
+  return {name, typeId}
+})
+export const StringField = Field(FT_STRING)
+export const BooleanField = Field(FT_BOOL)
+
+export const Collection = ({id, name, fields = [], items} = {}) => {
   const finalId = when(isNil, idGen.nextId)(id)
   return {
     id: finalId,
+    fields,
     name: defaultTo(finalId)(name),
     items: defaultTo([])(items),
   }
