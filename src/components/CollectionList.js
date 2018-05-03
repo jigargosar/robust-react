295acd3078d16531c4e18c7a21c83bed4d2f5aa2
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-03 13:44:50 +0530

    feat(Collections): extracted CollectionDetail

--- src/components/CollectionList.js
+++ src/components/CollectionList.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
+
 import {
-  Button,
+  Card,
   ExpansionPanel,
   ExpansionPanelDetails,
   ExpansionPanelSummary,
@@ -10,6 +11,9 @@ import {
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import {div, h, pre} from '../hyper-script'
 import {KeyedModels} from './KeyedModels'
+
+const CollectionDetail = ({collection}) =>
+  h(Card, [pre(JSON.stringify(collection, null, 2))])
 
 const CollectionExpansionPanel = withStyles({
   container: {
@@ -21,32 +25,25 @@ const CollectionExpansionPanel = withStyles({
   },
   item: {},
 })(({model: collection, classes}) =>
-  h(ExpansionPanel, {onClick: e => console.warn('ExpansionPanel', e)}, [
-    h(
-      ExpansionPanelSummary,
-      {
-        onClick: e => console.warn('ExpansionPanelSummary', e),
-        expandIcon: h(ExpandMoreIcon),
-      },
-      [
-        div({className: classes.container}, [
-          h(
-            Typography,
-            {className: classes.item, variant: 'subheading'},
-            collection.name,
-          ),
-          h(
-            Typography,
-            {className: classes.item, variant: 'caption'},
-            `(${collection.items.length})`,
-          ),
-          h(Button, {onClick: e => console.warn('Button', e)}, ['Hello']),
-        ]),
-      ],
-    ),
-    h(ExpansionPanelDetails, [pre(JSON.stringify(collection, null, 2))]),
+  h(ExpansionPanel, [
+    h(ExpansionPanelSummary, {expandIcon: h(ExpandMoreIcon)}, [
+      div({className: classes.container}, [
+        h(
+          Typography,
+          {className: classes.item, variant: 'subheading'},
+          collection.name,
+        ),
+        h(
+          Typography,
+          {className: classes.item, variant: 'caption'},
+          `(${collection.items.length})`,
+        ),
+      ]),
+    ]),
+    h(ExpansionPanelDetails, [h(CollectionDetail, {collection})]),
   ]),
 )
+
 export const CollectionList = ({collections}) =>
   h('div', [
     h(KeyedModels, {
