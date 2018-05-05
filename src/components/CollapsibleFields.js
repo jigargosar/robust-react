31702c03a74bb4d12d025e8ab3a21f969fda8f96
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-05 18:24:26 +0530

    feat(Collections): Extracted Collapsible Fields

---
+++ src/components/CollapsibleFields.js
@@ -0,0 +1,55 @@
+import {compose} from 'ramda'
+import Props from 'prop-types'
+import {setDisplayName, withHandlers, withStateHandlers} from 'recompose'
+import {Collapse, IconButton, Paper, Typography, withStyles} from 'material-ui'
+import {h} from '@jigargosar/utils'
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown'
+import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp'
+import {Fragment} from 'react'
+import {Fields} from './Fields'
+import {FlexRow} from './ui/FlexRow'
+
+const wrapDisplayName = require('recompose').wrapDisplayName
+
+const withToggleInner = compose(
+  withStateHandlers(
+    {toggleOn: false},
+    {toggle: ({toggleOn}) => () => ({toggleOn: !toggleOn})},
+  ),
+  withHandlers({ifToggleOn: ({toggleOn}) => (t, f) => (toggleOn ? t : f)}),
+)
+
+const withToggle = Base =>
+  setDisplayName(wrapDisplayName(Base, 'withToggle'))(withToggleInner(Base))
+
+const enhance = compose(
+  withStyles(() => ({
+    fieldsHeader: {
+      flex: 1,
+      marginLeft: 12,
+    },
+  })),
+  withToggle,
+  setDisplayName('CollapsibleFields'),
+)
+
+const CollapsibleFields = enhance(
+  ({collection, toggle, toggleOn, ifToggleOn, classes}) =>
+    h(Fragment, [
+      h(FlexRow, [
+        h(Typography, {className: classes.fieldsHeader, variant: 'title'}, [
+          'Fields',
+        ]),
+        h(IconButton, {onClick: toggle}, [
+          h(ifToggleOn(KeyboardArrowUp, KeyboardArrowDown)),
+        ]),
+      ]),
+      h(Paper, [h(Collapse, {in: toggleOn}, [h(Fields, {collection})])]),
+    ]),
+)
+
+CollapsibleFields.propTypes = {
+  className: Props.string,
+}
+
+export {CollapsibleFields}
--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,25 +1,20 @@
 import {compose} from 'ramda'
 import Props from 'prop-types'
 import {setDisplayName} from 'recompose'
-import {IconButton, Paper, Toolbar, Typography, withStyles} from 'material-ui'
+import {IconButton, Toolbar, Typography, withStyles} from 'material-ui'
 import cn from 'classnames'
 import {h} from '@jigargosar/utils'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
-import {Fields} from './Fields'
+import {CollapsibleFields} from './CollapsibleFields'
 
 const enhance = compose(
   withStyles(() => ({
     root: {},
     backButton: {
-      marginLeft: -12,
       marginRight: 20,
     },
-    title: {
-      // flex: 1,
-      // textAlign: 'center',
-    },
-    fieldsHeader: {
-      margin: 12,
+    toolbar: {
+      paddingLeft: 0,
     },
   })),
   setDisplayName('Collection'),
@@ -27,18 +22,13 @@ const enhance = compose(
 
 const Collection = enhance(({className, classes, collection, onGoBack}) =>
   h('div', {className: cn(classes.root, className)}, [
-    h(Toolbar, [
+    h(Toolbar, {className: classes.toolbar}, [
       h(IconButton, {className: classes.backButton, onClick: onGoBack}, [
         h(ArrowBackIcon),
       ]),
-      h(Typography, {className: classes.title, variant: 'title'}, [
-        collection.name,
-      ]),
+      h(Typography, {variant: 'title'}, [collection.name]),
     ]),
-    h(Typography, {className: classes.fieldsHeader, variant: 'title'}, [
-      'Fields',
-    ]),
-    h(Paper, [h(Fields, {collection})]),
+    h(CollapsibleFields, {collection}),
   ]),
 )
 
