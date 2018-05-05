5cb933c86a3fe8815aa1085348e23406bfe94ee4
Jigar Gosar <jigar.gosar@gmail.com>
2018-05-05 17:15:52 +0530

    feat(Collections): make table cell dense

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,22 +1,43 @@
 import {compose} from 'ramda'
 import Props from 'prop-types'
 import {setDisplayName} from 'recompose'
-import {Button, Paper, Typography, withStyles} from 'material-ui'
+import {IconButton, Paper, Toolbar, Typography, withStyles} from 'material-ui'
 import cn from 'classnames'
 import {h} from '@jigargosar/utils'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 import {Fields} from './Fields'
 
 const enhance = compose(
   withStyles(() => ({
     root: {},
+    backButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    title: {
+      // flex: 1,
+      // textAlign: 'center',
+    },
+    fieldsHeader: {
+      margin: 12,
+    },
   })),
   setDisplayName('Collection'),
 )
 
 const Collection = enhance(({className, classes, collection, onGoBack}) =>
   h('div', {className: cn(classes.root, className)}, [
-    h(Typography, {variant: 'title'}, [collection.name]),
-    h(Button, {onClick: onGoBack}, 'Back'),
+    h(Toolbar, [
+      h(IconButton, {className: classes.backButton, onClick: onGoBack}, [
+        h(ArrowBackIcon),
+      ]),
+      h(Typography, {className: classes.title, variant: 'title'}, [
+        collection.name,
+      ]),
+    ]),
+    h(Typography, {className: classes.fieldsHeader, variant: 'title'}, [
+      'Fields',
+    ]),
     h(Paper, [h(Fields, {collection})]),
   ]),
 )
